0d8da8975739e637e8ea9ccf381c6c4ca7819738
zephyr439 <zrc439@outlook.com>
2025-10-02 15:22:04 -0400

    Update intro to show Engs 4 Lab 1 link on new line

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -6,23 +6,28 @@ export function Intro() {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         R.Z's<br />Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        For HCI projects,{" "}
-        <a
-          href="/hci/redesign"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          visit here
-        </a>
-        . For Engs 4 Lab 1,{" "}
-        <a
-          href="/engs4/lab1"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          visit here
-        </a>
-        .
-      </h4>
+      <div className="text-center md:text-left text-lg mt-5 md:pl-8">
+        <h4 className="mb-2">
+          For HCI projects,{" "}
+          <a
+            href="/hci/redesign"
+            className="underline hover:text-blue-600 duration-200 transition-colors"
+          >
+            visit here
+          </a>
+          .
+        </h4>
+        <h4>
+          For Engs 4 Lab 1,{" "}
+          <a
+            href="/engs4/lab1"
+            className="underline hover:text-blue-600 duration-200 transition-colors"
+          >
+            visit here
+          </a>
+          .
+        </h4>
+      </div>
     </section>
   );
 }
